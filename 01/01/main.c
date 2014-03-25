64b4e4e71d345dbd8b1693f2144cb35cfdf40430
wangxiaokai <wangxiaokai1994@126.com>
2014-03-25 22:38:40 +0800

    Initial Commit

---
+++ 01/01/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  01
+//
+//  Created by 潇翔 汪 on 14-3-25.
+//  Copyright (c) 2014年 潇翔 汪. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
