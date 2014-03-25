09d4eb54e327cb34da447e0cd376d2d49b896c1a
wangxiaokai <wangxiaokai1994@126.com>
2014-03-25 22:46:41 +0800

    init

---
+++ 06/06/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  06
+//
+//  Created by 潇翔 汪 on 14-3-25.
+//  Copyright (c) 2014年 潇翔 汪. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    return 0;
+}
+
