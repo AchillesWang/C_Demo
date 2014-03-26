dfdb1fd8d0c3d8f5230cb976547d125a7699aa3c
wangxiaokai <wangxiaokai1994@126.com>
2014-03-26 14:13:39 +0800

    init

---
+++ 1/1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  1
+//
+//  Created by 潇翔 汪 on 14-3-26.
+//  Copyright (c) 2014年 潇翔 汪. All rights reserved.
+//
+
+#include <stdio.h>
+//输出“Hello，World”
+int main()
+{
+    printf("Hello World!\n");
+    
+}
